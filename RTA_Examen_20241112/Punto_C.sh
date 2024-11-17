059078f1a9cccf235f01e75c79027a6e15dd103d
emmanuelmartindavid <emmanuel.david.martin77@gmail.com>
2024-11-17 18:16:33 -0500

    feat: agrego comandos usados para desarrollo punto C Docker

---
+++ RTA_Examen_20241112/Punto_C.sh
@@ -0,0 +1,32 @@
+#COMANDOS DOCKER
+#docker login -u emmanuelmartindavid
+
+#docker build -t emmanueldavidmartin/web1-martin -f web1-martin .
+
+
+#sudo usermod -a -G docker $(whoami)
+#grep docker /etc/group
+#id
+#sudo systemctl status docker
+
+#ESTO VA EN WEB1-APELLIDO
+#FROM nginx:latest
+
+#COPY index.html /usr/share/nginx/html/index.html
+
+#RUN.SH:
+#docker run -d -p 8080:80 emmanuelmartindavid/web1-martin
+
+#docker login
+
+#docker push emmanuelmartindavid/web1-martin
+
+#http://localhost:8080/
+
+#docker stop 439166aac2a8
+#docker rm 439166aac2a8
+#./run.sh
+
+#docker ps
+
+
